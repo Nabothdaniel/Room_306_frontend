a4bbf24acc2bcd8326d7f988f814f9a2eb0ca56d
jgodwin10 <easypeasy725@gmail.com>
2024-04-21 14:41:00 +0100

    escort

--- src/pages/RegisterCard.jsx
+++ src/pages/RegisterCard.jsx
@@ -67,7 +67,7 @@ const RegisterCard = () => {
                 <p className="pt-8 text-3xl text-[#B29A9A]">Free</p>
 
                 <Link
-                  to={"/sign-up/escort"}
+                  to={"/signup/escort"}
                   className="bg-white w-[100%] text-center block py-4 font-semibold mt-8 mb-3 rounded-[35px]"
                 >
                   Register Here
@@ -104,7 +104,7 @@ const RegisterCard = () => {
                     <p className="pt-8 text-3xl text-[#B29A9A]">Free</p>
 
                     <Link
-                      to={"/sign-up/client"}
+                      to={"/signup/client"}
                       className="bg-white block text-center w-[100%] py-4 font-semibold mt-8 mb-6 md:mb-3 rounded-[35px]"
                     >
                       Register Here
