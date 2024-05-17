4218b2a9878a23830e178b59450bee4a911dd5da
jgodwin10 <easypeasy725@gmail.com>
2024-05-17 03:23:29 +0100

    CHANGES

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [VitePWA(manifestForPlugin), react()],
   server: {
     proxy: {
       // Proxy requests for images to a different server
       "/media": {
         target: "https://room35backend.onrender.com",
         changeOrigin: true,
-        secure: true,
         rewrite: (path) => path.replace(/^\/images/, ""),
       },
     },
   },
-  plugins: [react()],
 });
